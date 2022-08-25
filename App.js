f9f445376b35fbdacdad14a12f94e5131803f131
Zain Khalid <zainkhalid000@gmail.com>
2022-08-25 11:49:10 +0500

    re arrange code

--- App.js
+++ App.js
@@ -50,112 +50,81 @@ const getLocation=useLocation()
       title: "welcome",
       body: "You are using hamyk world's most famous platform",
       extra: "data"
-  });
-  GetAsyncData()
-  Notifications.registerRemoteNotifications();
-
+    });
+    GetAsyncData()
+    Notifications.registerRemoteNotifications();
     Notifications.events().registerNotificationReceivedForeground((notification: Notification, completion) => {
       Notifications.postLocalNotification({
         title: "welcome",
         body: "You are using hamyk world's most famous platform",
         extra: "data"
-    });
+      });
       completion({alert: false, sound: false, badge: false});
     });
-
     Notifications.events().registerNotificationOpened((notification: Notification, completion) => {
       console.log(`Notification opened: ${notification.payload}`);
       completion();
     });
-  // Notifications.registerRemoteNotifications();
+  },[])
 
-    // getAsyncData()
-    },[])
-    async function getAsyncData () {
-      const userid = await AsyncStorage.getItem('userid')
-      const token = await AsyncStorage.getItem('token')
-      let user_id=JSON.parse(userid)
-      if(token){
-        Start(user_id,token)
-      }
+async function getAsyncData () {
+  const userid = await AsyncStorage.getItem('userid')
+  const token = await AsyncStorage.getItem('token')
+  let user_id=JSON.parse(userid)
+  if(token){
+    Start(user_id,token)
+  }
+}
+
+async function VeryIntensiveTask(taskDataArguments){
+  const { delay } = taskDataArguments;
+  const { id } = taskDataArguments;
+  const { token } = taskDataArguments;
+  await new Promise(async (resolve) => {
+    var i = 0;
+    for (let i = 0; BackgroundJob.isRunning(); i++) {
+      Fetch_Notification(id,token)
+      await sleep(delay);
     }
-    async function VeryIntensiveTask(taskDataArguments)
-    {
-        const { delay } = taskDataArguments;
-        const { id } = taskDataArguments;
-        const { token } = taskDataArguments;
+  });
+}
 
-
-        await new Promise(async (resolve) => {
-            var i = 0;
-            for (let i = 0; BackgroundJob.isRunning(); i++) {
-                // })
-
-                Fetch_Notification(id,token)
-
-  //   Notifications.postLocalNotification({
-  //     title: "Welcome !",
-  //     body: "Welcome to hamyk a platform where you will be entertained",
-  //     extra: "data"
-  // });
-
-               await sleep(delay);
-              }
-        });
-    }
-    function Fetch_Notification (id,token){
-if(id&& token){
-  var myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${token}`);
-
-  var formdata = new FormData();
-  formdata.append("user_id", 11);
-
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: formdata,
-    redirect: 'follow'
-  };
-
-  fetch(`${BaseUrl}${EndPoints.getNotif}`, requestOptions)
+function Fetch_Notification (id,token){
+  if(id&& token){
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${token}`);
+    var formdata = new FormData();
+    formdata.append("user_id", 11);
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: formdata,
+      redirect: 'follow'
+    };
+    fetch(`${BaseUrl}${EndPoints.getNotif}`, requestOptions)
     .then(response => response.json())
     .then(result => {
-
       if(result.status === "300"){
-
-return null
+        return null
       }else{
-sendNotification(result.data)
+        sendNotification(result.data)
       }
     })
     .catch(error => console.log('error', error));
-}else{
+  }
 }
-    }
 
 function sendNotification (data){
-
-
-data.map((item)=>{
-
-
+  data.map((item)=>{
     Notifications.postLocalNotification({
       title: item.title,
       body: item.body,
       extra: "data"
     })
-
   })
-
 }
 
-
-
-
-function Start(id,token)
-
-{
+function Start(id,token){
   const Options = {
     taskName: 'hamyk',
     taskTitle: 'hamyk Running',
@@ -171,34 +140,25 @@ function Start(id,token)
         token:token
     },
     actions: '["Exit"]'
-};
-
-    BackgroundService.start(VeryIntensiveTask, Options);
+  };
+  BackgroundService.start(VeryIntensiveTask, Options);
 }
-
- return(
-   <NavigationContainer>
-    <Stack.Navigator initialRouteName="Splash"   screenOptions={{
-       headerShown: false
-    }} >
-     <Stack.Screen name="Login" component={Login} />
-     <Stack.Screen name="Register" component={Register} />
-     <Stack.Screen name="Home" component={Home} />
-     <Stack.Screen name="SinglePost" component={SinglePost} />
-     <Stack.Screen name="Search" component={Search} />
-     <Stack.Screen name="Recorder" component={Recorder} />
-     <Stack.Screen name="SaveVideo" component={SaveVideo} />
-     <Stack.Screen name="OtherUser" component={OtherUser} />
-     <Stack.Screen name="Splash" component={Splash} />
-
-     <Stack.Screen name="Tos" component={TOS} />
-     <Stack.Screen name="Policies" component={Policies} />
-   </Stack.Navigator>
- </NavigationContainer>
- )
-
- };
-
-
-
- export default App;
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{headerShown: false}} >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SinglePost" component={SinglePost} />
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="Recorder" component={Recorder} />
+        <Stack.Screen name="SaveVideo" component={SaveVideo} />
+        <Stack.Screen name="OtherUser" component={OtherUser} />
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Tos" component={TOS} />
+        <Stack.Screen name="Policies" component={Policies} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+};
+export default App;
