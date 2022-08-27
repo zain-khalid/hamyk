c659238bead164eead06718cf7c495ce5d16130b
Zain Khalid <zainkhalid000@gmail.com>
2022-08-27 14:38:26 +0500

    code alignment and add safe area video

--- components/Follow_Data/Following.js
+++ components/Follow_Data/Following.js
@@ -6,7 +6,8 @@ import {
   TextInput,
   Image,
   FlatList,
-  Modal
+  Modal,
+  SafeAreaView
 } from 'react-native';
 import styles from './Styles';
 import { useNavigation } from '@react-navigation/native';
@@ -58,7 +59,7 @@ useEffect(()=>{
 async function getAsyncData () {
   const userid = await AsyncStorage.getItem('userid')
   const token = await AsyncStorage.getItem('token')
-  let user_id=JSON.parse(userid) 
+  let user_id=JSON.parse(userid)
   if(token){
     setToken(token)
 setMyid(user_id)
@@ -88,7 +89,7 @@ var requestOptions = {
 fetch(`${BaseUrl}${endPoint}`, requestOptions)
   .then(response => response.json())
   .then(result => {
-    
+
     setFollowings(result)
     console.log(result)})
   .catch(error => console.log('error', error));
@@ -101,18 +102,18 @@ fetch(`${BaseUrl}${endPoint}`, requestOptions)
 function followUser(id){
   var myHeaders = new Headers();
   myHeaders.append("Authorization", `Bearer ${token}`);
-  
+
   var formdata = new FormData();
   formdata.append("follow_to", id);
   formdata.append("follow_by", myId);
-  
+
   var requestOptions = {
     method: 'POST',
     headers: myHeaders,
     body: formdata,
     redirect: 'follow'
   };
-  
+
   fetch(`${BaseUrl}${EndPoints.hitFollow}`, requestOptions)
     .then(response => response.json())
     .then(result => {
@@ -134,7 +135,7 @@ function followUser(id){
 
 
 const renderUser =({item})=>(
-<Pressable 
+<Pressable
 onPress={()=>{
   setOtherUser(true)
   setOtherUserId(item.id)
@@ -153,7 +154,7 @@ style={{width:38,height:38,margin:10,borderRadius:1000}}
   <View style={{flexDirection:'column'}}>
     <Text style={{color:"rgba(0,0,0,0.7)"}}>
 {item.firstname}
- 
+
 {item.lastname}
   </Text>
     <Text style={{fontSize:12}}>
@@ -163,19 +164,19 @@ style={{width:38,height:38,margin:10,borderRadius:1000}}
     </View>
     {route==="following"?
     item.id === String(myId) ?null:
-    <Pressable 
-    
+    <Pressable
+
     onPress={()=>{
-      
+
       setIsfollowing(!is_following)
       followUser(item.id)}}
       style={styles.FollowingBtn} >
         <Text style={{margin:8,color:"white",fontSize:12}}>{is_following === true ?"Following":"Follow"}</Text>
     </Pressable>
 :
-null  
+null
 }
-  
+
 </Pressable>
 )
 return(
@@ -185,35 +186,37 @@ return(
 
   // transparent={false}
   visible={state}  >
+  <SafeAreaView>
 
     <View style={styles.container}>
 <View style={styles.Header}>
 
-<MaterialIcons  
+<MaterialIcons
 style={{marginLeft:20}}
 onPress={()=>changeState()}
 name='arrow-back' size={25} color="black"/>
 <Text style={{marginRight:25,color:"black"}}>{route==="following"?"Followings":"Followers"}</Text>
     </View>
-  
+
 
     <View style={{marginTop:10}}>
 
     <FlatList
     data={followings}
     renderItem={renderUser}
-    
+
     />
     </View>
 
 
-    
+
         </View>
-        {showOtherUser === true ? 
-        
+        {showOtherUser === true ?
+
         <OtherUser showOtherUser={showOtherUser} HideOtherUser={HideOtherUser} Other_id={Other_id}/>
      :
      null }
+  </SafeAreaView>
         </Modal>
 )
 
