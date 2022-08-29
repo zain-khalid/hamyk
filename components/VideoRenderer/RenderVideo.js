63891a69c19564e752cea5bbb33939956bf83542
sarib-app <saribkhann84@gmail.com>
2022-08-29 23:24:51 +0500

    Added one more attribute in API call in LikeVideo function , attribute name is vuser_id

--- components/VideoRenderer/RenderVideo.js
+++ components/VideoRenderer/RenderVideo.js
@@ -65,23 +65,23 @@ const Icon_Size = 35
 async function fetchComments(id){
   var myHeaders = new Headers();
   myHeaders.append("Authorization", `Bearer ${AsynData.token}`);
-
+  
   var formdata = new FormData();
   formdata.append("video_id", id);
-
+  
   var requestOptions = {
     method: 'POST',
     headers: myHeaders,
     body: formdata,
     redirect: 'follow'
   };
-
+  
   fetch(`${BaseUrl}${EndPoints.commentlist}`, requestOptions)
     .then(response => response.json())
     .then(result => {
 
       setCommentList(result.comments)
-
+  
   })
     .catch(error => console.log('error', error));
 }
@@ -113,7 +113,7 @@ async function fetchComments(id){
 
 
 
-
+      
       const Buffer = require("buffer").Buffer;
 const images= Buffer.from((item.thumbnail), 'base64').toString('ascii')
 
@@ -125,33 +125,33 @@ const images= Buffer.from((item.thumbnail), 'base64').toString('ascii')
     const onPlayPausePress = () => {
       setPaused(!paused);
     };
-
+  
     const onLikedPress=()=>{
     setIsliked((prev)=>!prev)
     }
-
-
+  
+  
     const onComment =()=>{
       setComments(comments+1)
       // fetchComments()
 
   }
 
-
-
+  
+  
   const onLike =()=>{
     GenerateLikeNotification(item.user_id)
     setLikeCount(likeCount+1)
     LikeVide()
   }
-
+  
   const onDislike =()=>{
 
     if(likeCount>0){
       LikeVide()
       setLikeCount(likeCount-1)
     }
-
+  
   }
 
 
@@ -163,6 +163,8 @@ myHeaders.append("Authorization", `Bearer ${AsynData.token}`);
 var formdata = new FormData();
 formdata.append("user_id", AsynData.myId);
 formdata.append("video_id", item.id);
+formdata.append("vuser_id", item.user_id);
+
 
 var requestOptions = {
   method: 'POST',
@@ -183,99 +185,120 @@ fetch(`${BaseUrl}like`, requestOptions)
 
 
 function VideoOptionsAll(){
-  return(
-    <>
-      <View
-      style={[styles.optionsVideo,{top:5, alignItems: 'center'}]}
-      >
-      <View style={styles.OptionsVideoInnerWrapperI}>
-  {
+  return(<View style={styles.VideoOptionsContainer}>
+  
+  
+    <View
+    style={[styles.optionsVideo,{marginTop:20}]}
+    >
+     <View style={styles.OptionsVideoInnerWrapperI}>
+{
 
-    item.image ==="default"?
-    <Image source={profile}
-
-    style={{width:30,height:30,marginLeft:10}}
-    />
-  :
-
-  <Image source={{uri:`${EndPoints.ProfileUrl}${item.image}`}}
-
-  style={{width:30,height:30,marginLeft:10,borderRadius:1000}}
+  item.image ==="default"?
+  <Image source={profile}
+  
+  style={{width:30,height:30,marginLeft:10}}
   />
+:
 
-  }
-    <Text
-    onPress={()=>HideOtherUser()}
+<Image source={{uri:`${EndPoints.ProfileUrl}${item.image}`}}
+  
+style={{width:30,height:30,marginLeft:10,borderRadius:1000}}
+/>
 
-    style={{color:"white",fontSize:18,marginLeft:10}}>{item.username}</Text>
-  </View>
+}
+  <Text
+  onPress={()=>HideOtherUser()}
+  
+  style={{color:"white",fontSize:18,marginLeft:10}}>{item.username}</Text>
+</View>
+    
+     
+
+     <View style={[styles.IconWrapper,{marginRight:10}]}>
+<Pressable
+ onPress={() => {
+  setShouldShowReport(true)
+  setPaused(true)
+  
+  }}
+>
+<MaterialIcons 
+
+ name="more-vert" color="white" size={30} />
+</Pressable>
+     
+     </View>
+     
+     </View>   
+
+
+<View>
 
 
 
-      <View style={[styles.IconWrapper,{marginRight:10,}]}>
-  <Pressable
-  onPress={() => {
-    setShouldShowReport(true)
-    setPaused(true)
+     <Text
+     
+     style={{marginBottom:20,color:"white",marginLeft:10,textAlign:"left",width:"50%"}}
 
-    }}
-  >
-  <MaterialIcons
+     >{item.description}</Text>
 
-  name="more-vert" color="white" size={30} />
-  </Pressable>
 
-      </View>
+  <View
+    style={styles.optionsVideo}
+    >
+     <View style={styles.OptionsVideoInnerWrapper}>
+     <View style={[styles.IconWrapper,{marginLeft:10}]}>
 
-      </View>
-      {/* <Text
-      style={{marginBottom:20,color:"white",marginLeft:10,textAlign:"left",width:"50%",position:"absolute",bottom:0}}
-      >{item.description}</Text> */}
-      <View
-        style={[styles.optionsVideo, {bottom:50,}]}
-      >
-        <View style={styles.OptionsVideoInnerWrapper}>
-          <View
-            style={[styles.IconWrapper,{marginLeft:10}]}
-          >
-            <Icon
-              onPress={()=>{
-                onLikedPress()
-                isLiked===false?onLike():onDislike()
-              }}
-              name='heart'
-              size={Icon_Size}
-              color= {isLiked ===true? colors[0].primaryColor : "white"}
-            />
-            <Text style={styles.fontStyling}>{likeCount}</Text>
-          </View>
-          <View style={styles.IconWrapper}>
-            <MaterialIcon
-              onPress={async()=>{
-              // fetchComments(item.id)
-                setVidId(item.id)
-                setPaused(true)
-                setShowModal(true)
-              }}
-              name='comment-text'
-              size={Icon_Size}
-              color="white"
-            />
-            <Text style={styles.fontStyling}>{comments}</Text>
-          </View>
-        </View>
-        <View style={[styles.IconWrapper,{marginRight:10}]}>
-          <IonIcon
-            onPress={()=> onShare(item.video,item.id)}
-            name='send-sharp'
-            size={Icon_Size}
-            color="white"
-          />
-          <Text style={styles.fontStyling}>{state.Global_Paused === true ?1:0}</Text>
-        </View>
-      </View>
-    </>
-  )
+<Icon 
+onPress={()=>{
+onLikedPress()
+isLiked===false?onLike():onDislike()
+}}
+name='heart'
+size={Icon_Size}
+color= {isLiked ===true? colors[0].primaryColor : "white"}
+
+/>
+<Text style={styles.fontStyling}>{likeCount}</Text>
+</View>
+<View style={styles.IconWrapper}>
+
+<MaterialIcon 
+onPress={async()=>{
+// fetchComments(item.id)
+setVidId(item.id)
+setPaused(true)
+setShowModal(true)
+}
+}
+name='comment-text'
+size={Icon_Size}
+color="white"
+/>
+<Text style={styles.fontStyling}>{comments}</Text>
+</View>
+</View>
+    
+     
+
+     <View style={[styles.IconWrapper,{marginRight:10}]}>
+
+<IonIcon 
+onPress={()=> onShare(item.video,item.id)}
+name='send-sharp'
+size={Icon_Size}
+color="white"
+
+/>
+<Text style={styles.fontStyling}>{state.Global_Paused === true ?1:0}</Text>
+     </View>
+     
+     </View> 
+     </View>
+
+
+</View>)
 }
 
 
@@ -285,86 +308,84 @@ function VideoOptionsAll(){
 
 
   return(
-
+  
     <Pressable
-      onPress={()=>{
-        onPlayPausePress()
-      }}
+    onPress={()=>{
+      onPlayPausePress()
+    }}
+    
     >
-
-  <ImageBackground
+  
+  <ImageBackground 
   source={{uri:`${EndPoints.VideoBaseUrl}${item.thumbnail}`}}
-  resizeMode="stretch"
-
+  resizeMode="cover"
+  style={styles.ImgBg}
   >
-{/*
-   <Video
-    source={{uri:`${EndPoints.VideoBaseUrl}${item.video}`}}
-    paused={paused===true?paused:indexx===index?false:true}
-
+{/* 
+   <Video  
+    source={{uri:`${EndPoints.VideoBaseUrl}${item.video}`}}        
+    paused={paused===true?paused:indexx===index?false:true}   
+   
     playInBackground={false}
-    resizeMode="stretch"
-    style={styles.backgroundVideo}
-    repeat={true}
+    resizeMode="stretch"            
+    style={styles.backgroundVideo}  
+    repeat={true}      
     />  */}
 
 
 <VideoPlayer
-  repeat={true}
-  muted={false}
-  autoplay={true}
-  resizeMode="stretch"
-  hideControlsOnStart={true}
-  style={styles.backgroundVideo}
-  video={{uri:`${EndPoints.VideoBaseUrl}${item.video}`}}
-  paused={paused===true?paused:indexx===index?false:true}
+     video={{uri:`${EndPoints.VideoBaseUrl}${item.video}`}}        
+     paused={paused===true?paused:indexx===index?false:true}  
   thumbnail={{uri:`${EndPoints.VideoBaseUrl}${item.thumbnail}`}}
+  resizeMode="stretch"            
+  style={styles.backgroundVideo}  
+  autoplay={true}
+  controls={false}
+muted={false}
+repeat={true}
+hideControlsOnStart={true}
+
 />
 
 
 
 <Filters filterType={filter}/>
 
-
+  
   <VideoOptionsAll/>
- {showModal === true ?
+ {showModal === true ? 
 
- <Comment
- state={showModal}
- changeState={()=>onchangeState()}
+ <Comment 
+ state={showModal} 
+ changeState={()=>onchangeState()}   
   commentList={commentList}
    vidId={vidId}
-
+   
    onCommentSent={onComment}
    otheruserID={item.user_id}
    />
 :null
 }
 
-{showOtherUser === true ?
-
+{showOtherUser === true ? 
+        
         <OtherUser showOtherUser={showOtherUser} HideOtherUser={HideOtherUser} Other_id={item.user_id}/>
      :
      null }
 
 
-<ReportVideo
-
+<ReportVideo 
 showModal={showReport}
-
-OnSetModal={onSetReport}
-
+OnSetModal={onSetReport} 
 videoId={item.id}
-
-
 />
-
+       
   </ImageBackground>
 
   </Pressable>
-
-
-
+  
+  
+    
     )
                     }
   export default RenderVideo
